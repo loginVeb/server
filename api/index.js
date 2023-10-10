7ed4c4c2fd21c0fe7a43ec520002cc9ee1718d89
loginVeb <loginVeb@yandex.ru>
2023-10-10 19:39:23 +0300

    3

--- api/index.js
+++ api/index.js
@@ -12,10 +12,7 @@ const conn = createConnection({
     host:process.env.DB_HOST, 
     user:process.env.DB_USERNAME, 
     password:process.env.DB_PASSWORD, 
-    database:process.env.DB_DBNAME, 
-    waitForConnections:true, 
-    connectionLimit:10,
-    queueLimit:0
+    database:process.env.DB_DBNAME
 })
 
 conn.connect( (err) => { 
